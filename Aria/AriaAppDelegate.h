58b76eb8262565a2584cb15541a38198bf6e9367
defun <harsha@defun.org>
2010-10-17 21:38:33 -0700

    Aria Beginsgit add Aria

---
+++ Aria/AriaAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AriaAppDelegate.h
+//  Aria
+//
+//  Created by sriharsha Chandarreddy on 10/17/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AriaAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Aria/AriaAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  AriaAppDelegate.m
+//  Aria
+//
+//  Created by sriharsha Chandarreddy on 10/17/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "AriaAppDelegate.h"
+
+@implementation AriaAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
